825e1464ffe52e084daa80bfca7bf0782ff959a6
atom <sagi.a.tom@gmail.com>
2023-10-10 18:59:06 +0200

    torpedó implementáció javítás

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -80,7 +80,7 @@ public class GT4500 implements SpaceShip {
         // try to fire both of the torpedo stores
         //TODO implement feature
 
-        break;
+        return true;
     }
 
     return firingSuccess;
